6e5117e82394043a9f0d2afbe06b4c3436fb93bb
Slava Kukuyev <slava.kukuyev@gmail.com>
2023-11-06 14:48:41 +0200

    convert roman string to int (#1)

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	romantoint "github.com/slavakukuyev/leetcode-go/roman_to_int"
+)
+
+func main() {
+	fmt.Println(romantoint.RomanToInt("MCMXCIV")) //1994
+}
---
+++ roman_to_int/roman_to_int.go
@@ -0,0 +1,68 @@
+package romantoint
+
+import "regexp"
+
+var roman_map = map[byte]int{
+	'I': 1,
+	'V': 5,
+	'X': 10,
+	'L': 50,
+	'C': 100,
+	'D': 500,
+	'M': 1000,
+}
+
+func isValidRoman(s string) bool {
+	l := len(s)
+	if l < 1 || l > 15 {
+		return false
+	}
+
+	for i := 0; i < l; i++ {
+		if _, ok := roman_map[s[i]]; !ok {
+			return false
+		}
+	}
+
+	if l == 1 {
+		return true
+	}
+
+	invalidRegex := regexp.MustCompile(`IIII|XXXX|CCCC|MMMM|VV|LL|DD|IL|IC|ID|IXL|IM|XD|XM|LC|LD|LM|DM`)
+	return !invalidRegex.MatchString(s)
+}
+
+func RomanToInt(s string) int {
+	if !isValidRoman(s) {
+		return 0
+	}
+
+	l := len(s) - 1
+	end := false
+
+	var number int
+	for i := 0; i < l; i++ {
+
+		if (s[i] == 'I' && s[i+1] == 'V') ||
+			(s[i] == 'I' && s[i+1] == 'X') ||
+			(s[i] == 'X' && s[i+1] == 'L') ||
+			(s[i] == 'X' && s[i+1] == 'C') ||
+			(s[i] == 'C' && s[i+1] == 'D') ||
+			(s[i] == 'C' && s[i+1] == 'M') {
+			number += (roman_map[s[i+1]] - roman_map[s[i]])
+			i++
+			if i == l {
+				end = true
+			}
+			continue
+		}
+
+		number += roman_map[s[i]]
+
+	}
+	if !end {
+		number += roman_map[s[len(s)-1]]
+	}
+
+	return number
+}
